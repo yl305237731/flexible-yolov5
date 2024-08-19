c1773bd593662646a42a624f7e594b2fea58891d
Bobo~ <9928596+Bobo-y@users.noreply.github.com>
2024-08-19 10:36:03 +0800

    Update swin_transformer.py

--- od/models/backbone/swin_transformer.py
+++ od/models/backbone/swin_transformer.py
@@ -152,7 +152,8 @@ class WindowAttention(nn.Module):
 
         attn = self.attn_drop(attn)
 
-        x = (attn @ v).transpose(1, 2).reshape(B_, N, C)
+        # x = (attn @ v).transpose(1, 2).reshape(B_, N, C)
+        x = (attn.type_as(v) @ v).transpose(1, 2).reshape(B_, N, C) 
         x = self.proj(x)
         x = self.proj_drop(x)
         return x
